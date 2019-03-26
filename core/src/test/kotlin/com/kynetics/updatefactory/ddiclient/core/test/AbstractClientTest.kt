ee43f9b288d6ace1588d04d96552f8efdd1547e9
Daniele Sergio <daniele.sergio@kynetics.com>
2019-03-26 15:03:51 +0100

    Update test to use notification instead of delay

--- core/src/test/kotlin/com/kynetics/updatefactory/ddiclient/core/test/AbstractClientTest.kt
+++ core/src/test/kotlin/com/kynetics/updatefactory/ddiclient/core/test/AbstractClientTest.kt
@@ -2,42 +2,32 @@ package com.kynetics.updatefactory.ddiclient.core.test
 
 import com.kynetics.updatefactory.ddiclient.core.UpdateFactoryClientDefaultImpl
 import com.kynetics.updatefactory.ddiclient.core.api.*
+import com.kynetics.updatefactory.ddiclient.core.api.EventListener
 import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.basic
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.configDataProvider
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.defaultActionStatusOnStart
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.directoryDataProvider
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.downloadRootDirPath
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.endMessagesOnSuccessUpdate
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.filesDownloadedInOsWithAppsPairedToServerFile
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.firstActionEntry
 import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.gatewayToken
 import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.getDownloadDirectoryFromActionId
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.locationOfFileNamed
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.md5OfFileNamed
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.messagesOnSuccefullyDownloadAppDistribution
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.messagesOnSuccefullyDownloadOsDistribution
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.messagesOnSuccessfullyDownloadOsWithAppDistribution
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.startMessagesOnUpdateFond
 import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.tenantName
 import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.ufUrl
-import com.kynetics.updatefactory.ddiclient.core.test.TestUtils.updater
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import okhttp3.Credentials
+import kotlinx.coroutines.withTimeout
+import org.joda.time.Duration
 import org.testng.Assert
-import org.testng.annotations.DataProvider
-import org.testng.annotations.Test
 import java.io.File
+import java.util.*
 
 abstract class AbstractClientTest{
 
     protected  var client: UpdateFactoryClient? = null
 
+    private val queue = LinkedList<() -> Unit >()
+
     open protected fun defaultClientFromTargetId(
             directoryDataProvider:DirectoryForArtifactsProvider = TestUtils.directoryDataProvider,
             configDataProvider: ConfigDataProvider = TestUtils.configDataProvider,
             updater: Updater = TestUtils.updater,
+            eventListeners: List<EventListener> = emptyList(),
             deploymentPermitProvider: DeploymentPermitProvider = object : DeploymentPermitProvider{}): (String) -> UpdateFactoryClient = { targetId ->
         val clientData= UpdateFactoryClientData(
                 tenantName,
@@ -47,48 +37,71 @@ abstract class AbstractClientTest{
                 gatewayToken )
 
         val client = UpdateFactoryClientDefaultImpl()
+
+        val eventListener = object : EventListener {
+            override fun onEvent(event: EventListener.Event) {
+                when (event){
+
+                    is EventListener.Event.UpdateFinished, EventListener.Event.UpdateCancelled -> {
+                        queue.poll().invoke()
+                    }
+
+                    else -> { println(event) }
+                }
+            }
+        }
+
         client.init(
                 clientData,
                 directoryDataProvider,
                 configDataProvider,
                 deploymentPermitProvider,
-                emptyList(),
+                listOf(eventListener, *eventListeners.toTypedArray()),
                 updater
         )
         client
     }
 
+    //todo refactor test
+    protected fun testTemplate(deployment: TestUtils.TargetDeployments,
+                               timeout: Long = Duration.standardSeconds(15).millis,
+                               clientFromTargetId: (String) -> UpdateFactoryClient = defaultClientFromTargetId())  = runBlocking{
 
-    protected fun testTemplate(deployment: TestUtils.TargetDeployments, clientFromTargetId: (String) -> UpdateFactoryClient = defaultClientFromTargetId())  = runBlocking{
+        withTimeout(timeout){
+            client = clientFromTargetId(deployment.targetId)
+            val managementApi = ManagementClient.newInstance(ufUrl)
 
-        client = clientFromTargetId(deployment.targetId)
-        val managementApi = ManagementClient.newInstance(ufUrl)
+            deployment.deploymentInfo.forEach{ deploymentInfo ->
 
-        deployment.deploymentInfo.forEach{ deploymentInfo ->
+                var actionStatus = managementApi.getTargetActionStatusAsync(basic, deployment.targetId, deploymentInfo.actionId).await()
 
-            var actionStatus = managementApi.getTargetActionStatusAsync(basic, deployment.targetId, deploymentInfo.actionId).await()
+                Assert.assertEquals(actionStatus, deploymentInfo.actionStatusOnStart)
 
-            Assert.assertEquals(actionStatus, deploymentInfo.actionStatusOnStart)
+                queue.add {
+                    launch {
+                        delay(400)
+                        actionStatus = managementApi.getTargetActionStatusAsync(basic,deployment.targetId,deploymentInfo.actionId).await()
 
-            client?.startAsync()
+                        Assert.assertEquals(actionStatus.content, deploymentInfo.actionStatusOnFinish.content)
 
-            launch {
-                delay(4000) //todo replace with a client state listener
-                actionStatus = managementApi.getTargetActionStatusAsync(basic,deployment.targetId,deploymentInfo.actionId).await()
+                        deploymentInfo.filesDownloadedPairedWithServerFile.forEach{ (fileDownloaded, serverFile)->
+                            println(File(fileDownloaded).absolutePath)
+                            println(File(serverFile).absolutePath)
+                            Assert.assertEquals(File(fileDownloaded).readText(), File(serverFile).readText())
+                        }
 
-                Assert.assertEquals(actionStatus.content, deploymentInfo.actionStatusOnFinish.content)
+                        getDownloadDirectoryFromActionId(deploymentInfo.actionId.toString()).deleteRecursively()
+                    }
 
-                deploymentInfo.filesDownloadedPairedWithServerFile.forEach{ (fileDownloaded, serverFile)->
-                    println(File(fileDownloaded).absolutePath)
-                    println(File(serverFile).absolutePath)
-                    Assert.assertEquals(File(fileDownloaded).readText(), File(serverFile).readText())
                 }
 
-                getDownloadDirectoryFromActionId(deploymentInfo.actionId.toString()).deleteRecursively()
+            }
+            client?.startAsync()
+            launch {
+                while (queue.isNotEmpty()){
+                    delay(500)}
             }
 
         }
-
     }
-
 }
