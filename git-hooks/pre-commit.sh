8513691e6ef77bc92831bdb5cb26714dd8308fa2
Daniele Sergio <daniele.sergio@kynetics.com>
2019-08-12 11:06:24 +0200

    feat: Add deteck and ktlint to improve code quality
    Add deteckt to made static code analysis.
    Add Klint to check code style.
    Add gradle task to install git-hooks that they check code style
    and code quality before commit (klint) and push (detekt)
    
    Signed-off-by: Daniele Sergio <daniele.sergio@kynetics.com>

---
+++ git-hooks/pre-commit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+puts "Checking kotlin code style..."
+
+# stash any unstaged changes
+git stash -q --keep-index
+
+# check code respect kotlin style guide
+./gradlew checkStyle
+
+# store the last exit code in a variable
+RESULT=$?
+
+# unstash the unstashed changes
+git stash pop -q
+
+# return the './gradlew checkStyle' exit code
+exit ${RESULT}
