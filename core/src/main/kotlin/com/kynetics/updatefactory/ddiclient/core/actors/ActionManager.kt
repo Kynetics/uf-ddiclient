5e5b11805681bd3f31d81ef65bcbc0f51fb88edb
Daniele Sergio <daniele.sergio0@gmail.com>
2019-09-14 12:03:36 +0200

    refactor: simplify ActionManager
    Signed-off-by: Daniele Sergio <daniele.sergio0@gmail.com>

--- core/src/main/kotlin/com/kynetics/updatefactory/ddiclient/core/actors/ActionManager.kt
+++ core/src/main/kotlin/com/kynetics/updatefactory/ddiclient/core/actors/ActionManager.kt
@@ -33,59 +33,27 @@ private constructor(scope: ActorScope) : AbstractActor(scope) {
                 }
             }
 
-            msg is Out.DeploymentInfo && state.inDeployment(msg.info.id) -> child("deploymentManager")!!.send(msg)
-
-            msg is Out.DeploymentInfo && state.inDeployment -> child("deploymentManager")!!.send(Companion.Message.CancelForced)
-
-            msg is Out.DeploymentInfo -> {
-                val deploymentManager = actorOf("deploymentManager") { DeploymentManager.of(it) }
-                become(defaultReceive(state.copy(deployment = msg)))
-                deploymentManager.send(msg)
-                LOG.info("DeploymentInfo msg, decreased ping interval to be reactive on server requests (ping: 30s)")
-                connectionManager.send(In.SetPing(Duration.standardSeconds(30)))
-            }
+            msg is Out.DeploymentInfo -> onDeployment(msg,state)
 
             msg is DeploymentManager.Companion.Message.DownloadFailed -> {
                 become(defaultReceive(state.copy(deployment = null)))
                 child("deploymentManager")!!.close()
             }
 
-            msg is DeploymentManager.Companion.Message.UpdateFailed -> {
-                LOG.info("UpdateFailed.")
+            msg is DeploymentManager.Companion.Message.UpdateFailed ||
+                    msg is DeploymentManager.Companion.Message.UpdateFinished -> {
+                LOG.info(msg.javaClass.simpleName)
                 become(defaultReceive(state.copy(deployment = null)))
                 child("deploymentManager")!!.close()
                 LOG.info("Restore server ping interval")
                 connectionManager.send(In.SetPing(null))
             }
 
-            msg is DeploymentManager.Companion.Message.UpdateFinished -> {
-                LOG.info("UpdateFinished.")
+            msg is Out.DeploymentCancelInfo -> onCancelInfo(msg, state)
+
+            msg is Message.UpdateStopped -> {
+                LOG.info("update stopped")
                 become(defaultReceive(state.copy(deployment = null)))
-                child("deploymentManager")!!.close()
-                LOG.info("Restore server ping interval")
-                connectionManager.send(In.SetPing(null))
-            }
-
-            msg is Out.DeploymentCancelInfo && !state.inDeployment && registry.currentUpdateIsCancellable() -> {
-                connectionManager.send(In.CancelFeedback(
-                        CnclFdbkReq.newInstance(msg.info.cancelAction.stopId,
-                                CnclFdbkReq.Sts.Exc.closed,
-                                CnclFdbkReq.Sts.Rslt.Fnsh.success)))
-                notificationManager.send(MessageListener.Message.State.CancellingUpdate)
-                connectionManager.send(In.SetPing(null))
-            }
-
-            msg is Out.DeploymentCancelInfo && !registry.currentUpdateIsCancellable() -> {
-                connectionManager.send(ConnectionManager.Companion.Message.In.CancelFeedback(
-                        CnclFdbkReq.newInstance(msg.info.cancelAction.stopId,
-                                CnclFdbkReq.Sts.Exc.rejected,
-                                CnclFdbkReq.Sts.Rslt.Fnsh.success,
-                                "Update already started. Can't be stopped.")))
-            }
-
-            msg is Out.DeploymentCancelInfo -> {
-                LOG.warn("DeploymentCancelInfo")
-                child("deploymentManager")!!.send(msg)
                 LOG.info("Restore server ping interval")
                 connectionManager.send(In.SetPing(null))
             }
@@ -95,16 +63,7 @@ private constructor(scope: ActorScope) : AbstractActor(scope) {
                 child("deploymentManager")!!.send(Companion.Message.CancelForced)
             }
 
-            msg is Message.UpdateStopped -> {
-                LOG.info("update stopped")
-                become(defaultReceive(state.copy(deployment = null)))
-                LOG.info("Restore server ping interval")
-                connectionManager.send(In.SetPing(null))
-            }
-
-            msg is Out.NoAction -> {
-                notificationManager.send(MessageListener.Message.State.Idle)
-            }
+            msg is Out.NoAction -> onNoAction(msg, state)
 
             msg is ErrMsg -> {
                 LOG.warn("ErrMsg. Not yet implemented")
@@ -114,6 +73,62 @@ private constructor(scope: ActorScope) : AbstractActor(scope) {
         }
     }
 
+    private suspend fun onDeployment(msg: Out.DeploymentInfo, state:State){
+        when{
+            state.inDeployment(msg.info.id) -> child("deploymentManager")!!.send(msg)
+
+            state.inDeployment -> child("deploymentManager")!!.send(Companion.Message.CancelForced)
+
+            else -> {
+                val deploymentManager = actorOf("deploymentManager") { DeploymentManager.of(it) }
+                become(defaultReceive(state.copy(deployment = msg)))
+                deploymentManager.send(msg)
+                LOG.info("DeploymentInfo msg, decreased ping interval to be reactive on server requests (ping: 30s)")
+                connectionManager.send(In.SetPing(Duration.standardSeconds(30)))
+            }
+        }
+    }
+
+    private suspend fun onCancelInfo(msg: Out.DeploymentCancelInfo, state:State){
+        when{
+            !state.inDeployment && registry.currentUpdateIsCancellable() -> {
+                connectionManager.send(In.CancelFeedback(
+                        CnclFdbkReq.newInstance(msg.info.cancelAction.stopId,
+                                CnclFdbkReq.Sts.Exc.closed,
+                                CnclFdbkReq.Sts.Rslt.Fnsh.success)))
+                notificationManager.send(MessageListener.Message.State.CancellingUpdate)
+                connectionManager.send(In.SetPing(null))
+            }
+
+            !registry.currentUpdateIsCancellable() -> {
+                connectionManager.send(In.CancelFeedback(
+                        CnclFdbkReq.newInstance(msg.info.cancelAction.stopId,
+                                CnclFdbkReq.Sts.Exc.rejected,
+                                CnclFdbkReq.Sts.Rslt.Fnsh.success,
+                                "Update already started. Can't be stopped.")))
+            }
+
+            else -> {
+                LOG.warn("DeploymentCancelInfo")
+                child("deploymentManager")!!.send(msg)
+                LOG.info("Restore server ping interval")
+                connectionManager.send(In.SetPing(null))
+            }
+        }
+    }
+
+    private suspend fun onNoAction(msg:Out.NoAction, state:State){
+        when{
+            state.inDeployment -> {
+                LOG.warn("CancelForced/RemoveTarget.")
+                child("deploymentManager")!!.send(Companion.Message.CancelForced)
+            }
+
+            else -> {
+                notificationManager.send(MessageListener.Message.State.Idle)
+            }
+        }
+    }
     init {
         become(defaultReceive(State()))
         runBlocking { connectionManager.send(In.Register(channel)) }
