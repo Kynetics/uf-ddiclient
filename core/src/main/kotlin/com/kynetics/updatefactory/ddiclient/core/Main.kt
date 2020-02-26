92275c668ffb53d96c9e0da9ecae64480a31c68c
Daniele Sergio <daniele.sergio@kynetics.com>
2020-02-26 15:53:38 +0100

    feat: Configure example client via env variables
    Configure example client via env variables
    
    Signed-off-by: Daniele Sergio <daniele.sergio@kynetics.com>

--- core/src/main/kotlin/com/kynetics/updatefactory/ddiclient/core/Main.kt
+++ core/src/main/kotlin/com/kynetics/updatefactory/ddiclient/core/Main.kt
@@ -6,15 +6,21 @@ import com.kynetics.updatefactory.ddiclient.core.api.DirectoryForArtifactsProvid
 import com.kynetics.updatefactory.ddiclient.core.api.MessageListener
 import com.kynetics.updatefactory.ddiclient.core.api.UpdateFactoryClientData
 import com.kynetics.updatefactory.ddiclient.core.api.Updater
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.joda.time.Duration
 import java.io.File
 import java.security.DigestInputStream
 import java.security.MessageDigest
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.ObsoleteCoroutinesApi
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import org.joda.time.Duration
+import java.util.UUID
+
+
+
 
 suspend fun delay(duration: Duration) = delay(duration.millis)
 
@@ -28,12 +34,16 @@ fun File.md5(): String {
         while (digestInputStream.read(buffer) > -1) {}
         digestInputStream.close()
         digestInputStream.messageDigest
-                .digest()
-                .forEach {
-                    sb.append(String.format("%02X", it))
-                }
+            .digest()
+            .forEach {
+                sb.append(String.format("%02X", it))
+            }
     }
     return sb.toString().toLowerCase()
+}
+
+private fun env(envVariable:String, defaultValue:String):String{
+    return System.getenv(envVariable) ?: defaultValue
 }
 
 // TODO add exception handling ! --> A
@@ -41,57 +51,60 @@ fun File.md5(): String {
 fun main() = runBlocking {
     System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "TRACE")
 
-    var test = false
-
-    val clientData = UpdateFactoryClientData(
-            "ABA",
-            "test-rc2",
-            "https://stage.updatefactory.io",
+    (0 .. env("UF_CLIENT_POOL_SIZE", "1").toInt()).forEach {
+        val clientData = UpdateFactoryClientData(
+            env("UF_TENANT", "TEST"),
+            env("UF_CONTROLLER_ID", UUID.randomUUID().toString()),
+            env("UF_URL","https://stage.updatefactory.io"),
             UpdateFactoryClientData.ServerType.UPDATE_FACTORY,
-            "4625fea04ef2dea436c9fe342dbc2586")
+            env("UF_GATEWAY_TOKEN","")
+        )
 
-/*
-    val clientData= UpdateFactoryClientData(
-            "test",
-            "Target4",
-            "https://stage.updatefactory.io",
-            UpdateFactoryClientData.ServerType.UPDATE_FACTORY,
-            "528e9265ab4c7687ca2d8f933d3b77ec")
-*/
+        GlobalScope.launch {
+            getClient(clientData).startAsync()
+        }
+    }
+
+    while (true) {}
+}
+
+private fun getClient(clientData: UpdateFactoryClientData): UpdateFactoryClientDefaultImpl {
+    var test1 = false
     val client = UpdateFactoryClientDefaultImpl()
     client.init(
-            clientData,
-            object : DirectoryForArtifactsProvider { override fun directoryForArtifacts(): File = File(".") },
-            object : ConfigDataProvider {},
-            object : DeploymentPermitProvider {
-                override fun downloadAllowed(): Deferred<Boolean> {
-                    test = !test
-                    return CompletableDeferred(test)
-                }
-                override fun updateAllowed(): Deferred<Boolean> {
-                    test = !test
-                    return CompletableDeferred(test)
-                }
-            },
-            listOf(object : MessageListener {
-                override fun onMessage(message: MessageListener.Message) {
-                    println(message)
-                }
-            }),
-            object : Updater { override fun apply(modules: Set<Updater.SwModuleWithPath>, messenger: Updater.Messenger): Updater.UpdateResult {
+        clientData,
+        object : DirectoryForArtifactsProvider {
+            override fun directoryForArtifacts(): File = File(".${clientData.controllerId}")
+        },
+        object : ConfigDataProvider {},
+        object : DeploymentPermitProvider {
+            override fun downloadAllowed(): Deferred<Boolean> {
+                test1 = !test1
+                return CompletableDeferred(test1)
+            }
+
+            override fun updateAllowed(): Deferred<Boolean> {
+                test1 = !test1
+                return CompletableDeferred(test1)
+            }
+        },
+        listOf(object : MessageListener {
+            override fun onMessage(message: MessageListener.Message) {
+                println(message)
+            }
+        }),
+        object : Updater {
+            override fun apply(
+                modules: Set<Updater.SwModuleWithPath>,
+                messenger: Updater.Messenger
+            ): Updater.UpdateResult {
                 println("APPLY UPDATE $modules")
                 messenger.sendMessageToServer("Applying the update...")
                 Thread.sleep(1000)
                 messenger.sendMessageToServer("Update applied")
                 return Updater.UpdateResult(true)
-            } }
+            }
+        }
     )
-    Thread().run {
-        client.startAsync()
-        delay(5000)
-        client.forcePing()
-        delay(5000)
-        client.forcePing()
-    }
-    while (true) {}
+    return client
 }
