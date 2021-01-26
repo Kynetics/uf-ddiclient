8b60d57a393d6a875232fc98033b500eeae072bd
Daniele Sergio <daniele.sergio@kynetics.com>
2021-01-26 12:10:38 +0100

    docs: update copyright header
    Update copyright header to all source files.

--- tag.sh
+++ tag.sh
@@ -1,3 +1,12 @@
+#
+# Copyright © 2017-2021  Kynetics  LLC
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+
 # Tag last commit as 'latest'.
 
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
